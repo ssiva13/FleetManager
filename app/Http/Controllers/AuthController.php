aeda003377cafe6eac647f599a03a94ca39d4038
Simon Siva <simonsiva13@gmail.com>
2022-02-07 20:57:29 +0300

    add refresh token route and jwt config

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,7 +69,7 @@ class AuthController extends Controller
 		if($request->input('email')){
 			$credentials = $request->only(['email', 'password']);
 		}
-		if (! $token = auth()->attempt($credentials)) {
+		if ( !$token = auth()->attempt($credentials) ) {
 			return response()->json(['error' => 'Unauthorized'], 401);
 		}
 		return $this->respondWithToken($token);
--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@
 		$router->group(['prefix' => 'api'], function () use ($router) {
 			$router->post('register', 'AuthController@register');
 			$router->post('login', 'AuthController@login');
+			$router->get('refresh', 'AuthController@refresh');
 			$router->get('profile', 'AuthController@profile');
 			$router->get('users/{id}', 'UserController@singleUser');
 			$router->get('users', 'UserController@allUsers');
